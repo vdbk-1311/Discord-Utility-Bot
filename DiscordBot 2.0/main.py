f1e4179007a30e3c71736e494fa52c10fd2a7915
VĐBK <84456943+vdbk-1311@users.noreply.github.com>
2026-03-09 23:31:31 +0700

    Update main.py

--- DiscordBot 2.0/main.py
+++ DiscordBot 2.0/main.py
@@ -1,20 +1,30 @@
 import discord
 from discord.ext import commands
-import asyncio
 import os
+import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+@bot.event
+async def on_ready():
+    print(f"✅ Logged in as {bot.user}")
+
 async def main():
     async with bot:
-        await bot.load_extension("cogs.music")
+
+        for file in os.listdir("./cogs"):
+            if file.endswith(".py"):
+                await bot.load_extension(f"cogs.{file[:-3]}")
+
         await bot.start(TOKEN)
 
 asyncio.run(main())
