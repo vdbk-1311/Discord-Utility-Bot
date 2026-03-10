92ce2bab0dfd1f5fd73974ce7faa5670bb02f64f
VĐBK <84456943+vdbk-1311@users.noreply.github.com>
2026-03-10 15:05:33 +0700

    Delete DiscordBot 2.0 directory

--- DiscordBot 2.0/core/cache.py
+++
@@ -1,8 +0,0 @@
-class Cache:
-
-    def __init__(self):
-
-        self.guild_settings = {}
-        self.music_queue = {}
-
-cache = Cache()
--- DiscordBot 2.0/music/filters.py
+++
@@ -1,7 +0,0 @@
-def bass_boost():
-
-    return "-af bass=g=10"
-
-def nightcore():
-
-    return "-af asetrate=48000*1.25,aresample=48000,atempo=1.1"
